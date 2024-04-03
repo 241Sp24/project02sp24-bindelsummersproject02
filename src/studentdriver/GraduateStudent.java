10ba472c39ca6bd3fb0cc89ec663996bc9754e60
Hayden Bindel <124805187+hbindel26@users.noreply.github.com>
2024-04-02 21:27:50 -0500

    Classes
    I some what created the classes for the start

--- src/studentdriver/GraduateStudent.java
+++ src/studentdriver/GraduateStudent.java
@@ -1,13 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package studentdriver;
 
-/**
- *
- * @author S551669
- */
 public class GraduateStudent extends StudentFees {
 
+    private int coursesEnrolled;
+    private boolean isGraduateAssistant;
+    private String graduateAssistantType;
+    private double ADDITIONAL_FEES = 654.45;
+
+    public GraduateStudent(String studentName, int studentID, boolean isEnrolled, boolean isGraduateAssistant, String graduateAssistantType, int coursesEnrolled) {
+        super(studentName, studentID, isEnrolled);
+        this.isGraduateAssistant = isGraduateAssistant;
+        this.graduateAssistantType = graduateAssistantType;
+        this.coursesEnrolled = coursesEnrolled;
+
+    }
+
+    public GraduateStudent(String studentName, int studentID, boolean isEnrolled, boolean isGraduateAssistant, int coursesEnrolled) {
+        super(studentName, studentID, isEnrolled);
+        this.isGraduateAssistant = isGraduateAssistant;
+        this.coursesEnrolled = coursesEnrolled;
+    }
+
+    /**
+     * @return the coursesEnrolled
+     */
+    public int getCoursesEnrolled() {
+        return coursesEnrolled;
+    }
+
+    /**
+     * @return the isGraduateAssistant
+     */
+    public boolean isIsGraduateAssistant() {
+        return isGraduateAssistant;
+    }
+
+    public double getPayableAmount() {
+        if (this.graduateAssistantType.equals("full")) {
+            return (((super.PayableAmount() * getCoursesEnrolled()) * 0) + this.ADDITIONAL_FEES);
+        } else if (this.graduateAssistantType.equals("half")) {
+            return (((super.PayableAmount() * getCoursesEnrolled()) / 2) + this.ADDITIONAL_FEES);
+        } else {
+            return ((super.PayableAmount() * getCoursesEnrolled()) + this.ADDITIONAL_FEES);
+        }
+
+    }
+
+    public String toString() {
+        return super.toString() + "\nGraduate assistant: " + this.isGraduateAssistant + "\nGradaute assistant type: " + this.graduateAssistantType + "\nCourses enrolled: " + this.coursesEnrolled + "\n Payable amount: " + getPayableAmount();
+    }
 }
--- src/studentdriver/UGStudent.java
+++ src/studentdriver/UGStudent.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package studentdriver;
 
-/**
- *
- * @author S551669
- */
 public class UGStudent extends StudentFees {
 
+    private double scholarshipAmount;
+    private int coursesEnrolled;
+    private boolean hasScholarship;
+    private double ADDITIONAL_FEE = 820.70;
+
+    public UGStudent(String studentName, int studentID, boolean isEnrolled, boolean hasScholarship, double scholarshipAmount, int coursesEnrolled) {
+        super(studentName, studentID, isEnrolled);
+        this.hasScholarship = hasScholarship;
+        this.scholarshipAmount = scholarshipAmount;
+        this.coursesEnrolled = coursesEnrolled;
+    }
+
+    /**
+     * @return the coursesEnrolled
+     */
+    public int getCoursesEnrolled() {
+        return coursesEnrolled;
+    }
+
+    /**
+     * @return the hasScholarship
+     */
+    public boolean isHasScholarship() {
+        return hasScholarship;
+    }
+
+    /**
+     * @return the scholarshipAmount
+     */
+    public double getScholarshipAmount() {
+        return scholarshipAmount;
+    }
+
+    public double getPayableAmount() {
+        return ((super.PayableAmount() * getCoursesEnrolled()) + this.ADDITIONAL_FEE) - getScholarshipAmount();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nScholarship: " + this.hasScholarship + "\nScholarship amount: " + this.getScholarshipAmount() + "\n Courses enrolled: " + this.coursesEnrolled + "\n Payable amount: " + getPayableAmount();
+    }
 }
